da176e6f8b08ef799a3afd9824689d912b9968ae
St Madeleines IT Trainee <stmadeleinesittrainee@Sts-MacBook-Air.local>
2025-06-20 13:30:21 +1000

    patching XSS vulns through CSP

--- myApp.js
+++ myApp.js
@@ -14,11 +14,10 @@ app.use(helmet.dnsPrefetchControl());
 app.use(helmet.noCache());
 app.use(helmet.contentSecurityPolicy({
   directives: {
-  defaultSrc: ["'self'"],
-  scriptSrc: ['self', 'trusted-cdn.com'],
+    defaultSrc: ["'self'"],
+    scriptSrc: ["'self'", 'trusted-cdn.com']
   }
-})
-);
+}));
 
 
 
